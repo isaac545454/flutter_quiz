c1d5343bb6ec1647a6e5547d46eeea9823cc82cb
isaac545454 <isaac.newton1221@gmail.com>
2023-03-11 08:42:56 -0300

    feat: add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new Quiz());
-}
+void main() => runApp(new Quiz());
+
+final List<String> question = [
+  "qual é sua cor favorita?",
+  "qual é seu animal favorito?"
+];
 
 class Quiz extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Olá flutter"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Perguntas",
+            key: key,
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(question[0], key: key),
+            ElevatedButton(
+              onPressed: () => {},
+              child:
+                  const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              child:
+                  const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              child:
+                  const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              child:
+                  const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
