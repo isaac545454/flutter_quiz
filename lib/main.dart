d1d35721a00c509c71cd16fd97188f50678c196f
isaac545454 <isaac.newton1221@gmail.com>
2023-03-14 06:53:44 -0300

    feat: create List

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'question.dart';
@@ -5,9 +7,19 @@ import 'button.dart';
 
 void main() => runApp(new Quiz());
 
-final List<String> question = [
-  "qual é sua cor favorita?",
-  "qual é seu animal favorito?"
+final List<Map<String, Object>> question = [
+  {
+    'text': 'qual é sua cor favorita?',
+    'res': ['vermelho', 'azul', 'preto', 'branco']
+  },
+  {
+    'text': 'qual é seu animal favorito?',
+    'res': ['cachorro', 'gato', 'coelho', 'passarinho']
+  },
+  {
+    'text': 'qual sua linguagem de programação favorita?',
+    'res': ['python', 'typescript', 'Dart', 'Rust']
+  }
 ];
 
 class _ToRespondAppState extends State<Quiz> {
@@ -17,9 +29,9 @@ class _ToRespondAppState extends State<Quiz> {
     setState(() {
       _toRespondSelected++;
     });
-
-    print(_toRespondSelected);
   }
+
+  // dynamic to = question[_toRespondSelected]['text'];
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +39,15 @@ class _ToRespondAppState extends State<Quiz> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
-            "Perguntas",
+            'Perguntas',
           ),
         ),
         body: Column(
           children: <Widget>[
-            Question(question[_toRespondSelected]),
-            ResponseQuestion("Resposta um", _toRespond),
-            ResponseQuestion("resposta dois", _toRespond),
-            ResponseQuestion("resposta tres", _toRespond),
+            Question(question[_toRespondSelected]["text"].toString()),
+            ResponseQuestion('Resposta um', _toRespond),
+            ResponseQuestion('resposta dois', _toRespond),
+            ResponseQuestion('resposta tres', _toRespond),
           ],
         ),
       ),
